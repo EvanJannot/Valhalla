6f07177ffaddb072581a3da70393e5df0005d3ae
Evan Jannot <ejannot@ensc.fr>
2021-04-12 10:11:46 +0200

    maj

---
+++ Valhalla/Systems/Pause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RLNET;
+
+namespace RogueSharpRLNetSamples.Systems
+{
+    public class Pause
+    {
+        private readonly Queue<string> _lines;
+        public Pause()
+        {
+            _lines = new Queue<string>();
+        }
+
+        public void Add(string message)
+        {
+            _lines.Enqueue(message);
+            if (_lines.Count > 9)
+            {
+                _lines.Dequeue();
+            }
+        }
+
+        public void Draw(RLConsole console)
+        {
+            console.Clear();
+            string[] lines = _lines.ToArray();
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                console.Print(1, i + 1, lines[i], RLColor.White);
+            }
+        }
+    }
+}
